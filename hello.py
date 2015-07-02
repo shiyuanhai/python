b83ea2c4da180108f246c3356a5aae585ae3c1a2
Yuanhai Shi <shiyuanhai@gmail.com>
2015-07-01 22:45:17 -0400

    py introduction
    print, data types, variables

---
+++ hello.py
@@ -0,0 +1,23 @@
+print('hello world')
+print('Have','a','nice','day')
+print('100 + 200 =',100+200)
+
+# name = input('input your name: ')
+
+# print absolute value for variable
+a = -100
+if a >= 0 :
+	print(a)
+else :
+	print(-a)
+
+#data type and variables
+print("I\'m \"OK\"")
+print(r'Hello, "Bart"')
+print(r'\\t\\')
+print(True and False)
+
+a = 'abc'
+b = a
+a = 'xyz'
+print(b)
